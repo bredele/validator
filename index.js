56ed48282562babd258e08d9e63fd0bac892a058
bredele <olivier.wietrich@gmail.com>
2013-10-26 23:06:54 -0600

    add first version with example

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 
 /**
+ * Dependencies
+ */
+
+var Doors = require('doors');
+
+
+/**
  * Expose 'Validator'
  */
 
@@ -12,5 +19,34 @@ module.exports = Validator;
  */
 
 function Validator() {
-  //do something
+	this.validations = {};
+	this.doors = new Doors('validator');
 }
+
+
+/**
+ * Add validation.
+ * 
+ * @param {String}   name
+ * @param {Function} callback
+ * @param {Boolean}   required 
+ * @api public
+ */
+
+Validator.prototype.add = function(name, callback, required) {
+	this.validations[name] = callback;
+	if(required) this.doors.add(name);
+};
+
+
+/**
+ * Set 
+ * @param {String} name  
+ * @param {Any} value
+ * @api public
+ */
+
+Validator.prototype.set = function(name, value) {
+	this.doors.toggle(name, this.validations[name](value));
+};
+
